edb70b52e3859892ae9897b9648a3343ed68657a
Ben Ahlbrand <bmahlbrand@gmail.com>
2019-02-05 01:52:09 -0500

    NetworkConfig

---
+++ Assets/TrackrCore/NetworkConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public struct NetworkConfig 
+{
+    public string hostname;
+    public ushort port;
+
+    public NetworkConfig(string hostname, ushort port)
+    {
+        this.hostname = hostname;
+        this.port = port;
+    }
+}
